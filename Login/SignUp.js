8c490f6e88bde702e631b9a490be7b6e1732b89c
waqar amjad <waqaramjad345@gmail.com>
2018-06-10 12:29:56 +0500

    login module done but an error occur of transition

--- Login/SignUp.js
+++ Login/SignUp.js
@@ -7,24 +7,93 @@ import {ButtonRoundBlue, IconInput} from "@controls";
 import {Actions} from "react-native-router-flux";
 
 export default class SignUp extends Component {
+
+	constructor(props){
+		super(props)
+		this.state={
+			userName:'',
+			userEmail:'', 
+			userPassword:''				
+		}
+	}
+
+
+  userRegister = () =>{
+		//alert('ok'); // version 0.48
+		
+		const {userName} = this.state;
+		const {userEmail} = this.state;
+		const {userPassword} = this.state;
+		
+		
+		fetch('https://hardeepwork.000webhostapp.com/react/register.php', {
+			method: 'post',
+			header:{
+				'Accept': 'application/json',
+				'Content-type': 'application/json'
+			},
+			body:JSON.stringify({
+				name: userName,
+				email: userEmail,
+				password: userPassword,
+			})
+			
+		})
+		.then((response) => response.json())
+			.then((responseJson) =>{
+				alert(responseJson);
+			})
+			.catch((error)=>{
+				console.error(error);
+			});
+		
+	}
+	const styles = StyleSheet.create({
+		container: {
+			flex: 1,
+			justifyContent: 'center',
+			alignItems: 'center',
+			backgroundColor: '#F5FCFF',
+		},
+		welcome: {
+			fontSize: 20,
+			textAlign: 'center',
+			margin: 10,
+		},
+		instructions: {
+			textAlign: 'center',
+			color: '#333333',
+			marginBottom: 5,
+		},
+	});
+	
+
+
+
+
   render() {
     return (
       <ScrollView>
         <View style={styles.container}>
           <View style={{marginTop: -20}}>
-            <IconInput placeholder="Name" image={require("@images/icon-password.png")}/>
-            <IconInput placeholder="Email" image={require("@images/icon-email.png")}/>
+            <IconInput   onChangeText= {userName => this.setState({userName})}
+ placeholder="Name" image={require("@images/icon-password.png")}/>
+
+            <IconInput 	  onChangeText= {userEmail => this.setState({userEmail})}
+            placeholder="Email" image={require("@images/icon-email.png")}/>
+
             <IconInput placeholder="Phone" image={require("@images/icon-phone.png")}/>
-            <IconInput placeholder="Password" image={require("@images/icon-password.png")}/>
+            
+            <IconInput  onChangeText= {userPassword => this.setState({userPassword})} placeholder="Password" image={require("@images/icon-password.png")}/>
           </View>
           <ButtonRoundBlue
-            onPress={() => Actions.home}
-            text="Registration"/>
+           onPress={this.userRegister}
+            text="osidjoiRegistration"/>
 
           <Text style={{color: 'white', opacity: 0.7, marginRight: 5, fontSize: 15}}>
             already have an account
           </Text>
-          <TouchableOpacity onPress={Actions.login} style={{alignSelf: 'flex-end', marginRight: 15}}>
+          <TouchableOpacity onPress={this.userRegister} style={{alignSelf: 'flex-end', marginRight: 15}}>
             <Text style={styles.registerLink}>
               Sign in now
             </Text>
--- SignIn.js
+++ SignIn.js
@@ -1,28 +1,109 @@
 'use strict';
 import React, {Component} from "react";
-import {Text, View, Dimensions} from "react-native";
+// import {Text, View, Dimensions} from "react-native";
+import { StyleSheet, View, TouchableOpacity, Dimensions, StatusBar, Image, Text, Button, Modal } from 'react-native';
+
 import {Actions} from "react-native-router-flux";
 import styles from "./styles";
 import {ButtonRoundBlue, IconInput}  from "@controls";
+import { Navigator } from 'react-native-deprecated-custom-components'
 
 export default class SignIn extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: 'Username',
-      password: '',
-      visibleHeight: Dimensions.get('window').height,
-      scroll: false
-    };
-  }
+ 
+  constructor(props){
+		super(props)
+		this.state={
+			userEmail:'',
+			userPassword:''
+		}
+	}
+
+ 
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     username: 'Username',
+  //     password: '',
+  //     visibleHeight: Dimensions.get('window').height,
+  //     scroll: false
+  //   };
+  // }
+
+	login = () =>{
+		const {userEmail,userPassword} = this.state;
+		let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+		if(userEmail==""){
+			//alert("Please enter Email address");
+		  this.setState({email:'Please enter Email address'})
+			
+		}
+		
+		else if(reg.test(userEmail) === false)
+		{
+		//alert("Email is Not Correct");
+		this.setState({email:'Email is Not Correct'})
+		return false;
+		  }
+
+		else if(userPassword==""){
+		this.setState({email:'Please enter password'})
+		}
+		else{
+		
+		fetch('https://hardeepwork.000webhostapp.com/react/login.php',{
+			method:'post',
+			header:{
+				'Accept': 'application/json',
+				'Content-type': 'application/json'
+			},
+			body:JSON.stringify({
+				// we will pass our input data to server
+				email: userEmail,
+				password: userPassword
+			})
+			
+		})
+		.then((response) => response.json())
+		 .then((responseJson)=>{
+			 if(responseJson == "ok"){
+				 // redirect to profile page
+				 alert("Successfully Login");
+				//  this.props.navigation.navigate("Profile");
+				this.props.navigator.push({
+					title: 'Search'
+			})
+
+			 }else{
+				 alert("Wrong Login Details");
+			 }
+		 })
+		 .catch((error)=>{
+		 console.error(error);
+		 });
+		}
+		
+		
+		// Keyboard.dismiss();
+	}
+
+	check=()=>{
+console.log('hy'+this)
+		this.props.navigator.push({
+			title: 'Search'
+	})
+
+	}
+
 
   render() {
     return (
       <View style={styles.container}>
         <View style={{marginTop: -15}}>
-          <IconInput placeholder="Username" image={require("@images/icon-user.png")}/>
+          <IconInput 	onChangeText={userEmail => this.setState({userEmail})}
+ placeholder="Username" image={require("@images/icon-user.png")}/>
 
-          <IconInput placeholder="Password" image={require("@images/icon-password.png")}/>
+          <IconInput 	onChangeText={userPassword => this.setState({userPassword})}
+ placeholder="Password" image={require("@images/icon-password.png")}/>
         </View>
 
         <View style={{flexDirection: 'row', alignSelf: 'flex-end', marginTop: 10, marginBottom: 10}}>
@@ -31,7 +112,8 @@ export default class SignIn extends Component {
           </Text>
         </View>
 
-        <ButtonRoundBlue text="Enter" onPress={() => Actions.home({data: this.state.value})}/>
+        <ButtonRoundBlue text="Enter" onPress={() => this.check()}
+/>
       </View>
     );
   }
